028f7fafb5477bb606dc0b28419169669c7e5ba2
youyix <youyis2fox@gmail.com>
2014-07-26 12:17:38 +0200

    newui

--- webapps/pages/script.js
+++ webapps/pages/script.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
 	// # This funciton below is only to test the exist of jQuery.
-	$("h1").click(function(){
-		$(this).css("color", "red");
-	});
+	// $("h1").click(function(){
+	// 	$(this).css("color", "red");
+	// });
 
 	// # When the 'login button clicked', script below is called.
 	$("#button").click(function(){
-	    // $.get("127.0.0.1:8080/login/?name=ghouan&ip=127.0.0.1", function(data, status){ 
-	    //Xing:发送get请求的时候要把自己的名字加上，同时还要把本机的ip地址加上，再发送
-            //就像这样 url:"/WEB-INF/login.xml?name="+getName()+"&ip="+getIP()
-		//getName()收集用户输入的姓名 getIp()想办法搞到机器的ip地址
+		$name = $("#name").val();
  	    $.ajax({
    	    	type: "GET",
-                url: "/WEB-INF/login.xml?name=ghouan&ip=1.1.1.1",
+            url: "/WEB-INF/login.xml?name={0}&ip={1}".f(getName(), getIp()),
            	dataType: "xml",
-          	success: update
-            });
+          	success: update,
+          	error: loginError
+        });
+        console.log("Name:{0}\nIP:{1}".f(getName(), getIp()));
   	});
 
 	// # 
@@ -23,9 +22,37 @@ $(document).ready(function(){
 		submitAuction();
 	});
 
+	$("#name").focus(function() {
+		if ( $(this).val() === "input your name" ) {
+			$(this).val("");
+		}
+	});
+
+
 });
 
+function getName() {
+	return $name;
+}
+
+function getIp() {
+	$ip = "127.0.0.1";
+	$.ajax({
+	    type: "GET",
+        url: "http://jsonip.com",
+       	dataType: "json",
+       	async: false,
+      	success: function(res) {
+      		$ip = res.ip;
+      	},
+      	error: function(res) {
+      	}
+    });
+    return $ip;
+}
+
 // Helper function for formatting strings
+// Usage: "sasdsdad{0}das{1}".f("ads", 2);
 String.prototype.format = String.prototype.f = function() {
     var s = this,
         i = arguments.length;
@@ -36,87 +63,113 @@ String.prototype.format = String.prototype.f = function() {
     return s;
 };
 
+// Helper function: convert string to number
+function S2N(str) {
+	return parseInt(str);
+}
+
 function loginError() {
 	$(".login_error").show();
 	// other ....
 }
 
 function update(data) {
-	$("#login").hide();
+	$("#login_box").hide();
 	console.log("update()");
+
+	console.log(data);
+
 	var $context = ($(data)).find("auction_context");
 	var $roundNumber = $context.children("round").attr("value");
 	var $isFinal = $context.children("round").attr("final");
-	alert("this roundnumber is:"+$roundNumber);
+	var $minIncreament = $context.children("minimum_increament").attr("value");
+	$("#bid_table").data("minIncreament", $minIncreament);
+
+	// alert("this roundnumber is:" + $roundNumber);
 	
 
 	console.log("Round {0}  Final? {1}".f($roundNumber, $isFinal));
 
-	$("#round_infomation").text(function() {
-		return "Round {0}  Final? {1}".f($roundNumber, $isFinal);
-	});
+	$("#round_infomation").html("<p>Round {0}  {1}  Min_increament {2}</p>"
+		.f($roundNumber, $isFinal==="yes" ? "<b class='alert'>Final!</b>" : "", $minIncreament));
 	
-	$("bid_table").find("tbody").find("tr").remove();
+	setTimer($context.children("duration").attr("value"));
+
+	$("#bid_table").find("tbody").find("tr").remove();
 
 	$context.find("item").each(function(i) {
 		console.log(this);
-		var $itemId = $(this).attr("id");
+		var $itemId = i;
 		var $itemName = $(this).attr("name");
 		var $price = $(this).attr("price");
 		var $owner = $(this).attr("owner");
 
+
 		console.log($itemId, $itemName, $price, $owner);
 
-		var $_id = $("<th class=\"invisible\"></th>").text($itemId);
+		var $_id = $("<th class='invisible'></th>").text($itemId);
 		var $_name = $("<th></th>").text($itemName);
 
 
 		var $_price = $("<th></th>").text($price);
-		var $_yprice = $("<th></th>").append($("<input type=\"text\" id=\"price{0}\"></input>".f($itemId)));
+		var $_yprice = $("<th></th>").append($("<input type='text' id='price{0}' class='input_price' value='{1}'></input>"
+			.f($itemId, S2N($minIncreament)+S2N($price))));
+		$_yprice.append($("<p id=price{0}_tips class='input_price_tips'></p>".f($itemId)));
+
 		var $item = $("<tr></tr>").append($_id, $_name, $_price, $_yprice);
 
 		$("#bid_table").find("tbody").append($item);
+
+		// validate
+
+		
 	});	
 
-	// Leave for timer
-	// ...
+	$(".input_price").change(function() {
+		// console.log("hi", $(this), "gh");
+		validateInput($(this));
+	});
+
+	$('#bid_table tbody tr').hover(function() {
+
+        $(this).addClass('zhover');
+    }, function() {
+
+        $(this).removeClass('zhover');
+    });
 
 	$("#bid").show();
 }
 
-// <bid>
-// 	<item_list>
-// 		<item id="0" name="dogfood" price="120" owner=""/>
-// 		<item id="1" name="kennel" price="290" owner=""/>
-// 		<item id="2" name="dota" price="0" owner=""/>
-// 		<!-- 价格为0表示没有对本品进行报价，或者放弃报价-->
-// 	</item_list> 
-// </bid>
 
-//
+function setTimer(timeToCount) {
+	$count = timeToCount;
+	$timer = setInterval(function() {
+		$("#timer").text("Time out after {0} seconds.".f($count));
+		$count = $count - 1;
+
+		if ( $count == 0 ) {
+			submitAuction();
+		}
+	}, 1000);
+}
 
 function collectData() {
-	var $xmlData = $("<div></div>");
-	var $bid = $("<bid></bid>");
-	var $itemList = $("<item_list></item_list>");
-	
+	var $bid = "";
+	var $items = "";
+
 	$("#bid_table").find("tbody").find("tr").each(function(i) {
 		
 		var $id = $(this).children("th:eq(0)").text();
 		var $name = $(this).children("th:eq(1)").text();
-		var $price = $("#price{0}".f($id)).val();
-		var $owner = "";
-		console.log($id, $name, $price, $owner);
+		var $yprice = $("#price{0}".f($id)).val();
+		var $owner = " ";
 
-		var $temp = $("<item id={0} name={1} price={2} owner={3}></item>".f($id, $name, $price, $owner));
-		$itemList.append( $temp );
-		
+		$items = $items + "<item name={0} price={1} owner={2}></item>".f($name, $yprice, $owner);
 	});
-	$bid.append($itemList);
-	$xmlData.append($bid);
-
-	console.log($xmlData.text());
-	return $xmlData.text();
+	
+	$bid = "<bid><item_list>" + $items + "</item_list></bid>";
+	return $bid;
 }
 
 
@@ -124,12 +177,34 @@ function updateError() {
 
 }
 
+
+// Post xml to server
 function submitAuction() {
 
 	console.log("submitAuction");
+
+	if ( ! validateAllInput() ) {
+		if ( isTimeUp() ) {
+			setAll2Valid();		
+		} else {
+			return;
+		}
+	}
+
+	$(".input_price").removeClass("invalid_input");
+	$(".input_price").removeClass("invalid_price");
+	
+
+	$(".input_price").next("p").hide();
+
+	$("#timer").text("Your bid is being submitting...");
 	// collect data
 	var $xmlData = collectData();
 	
+	clearInterval($timer);
+
+	console.log($xmlData);
+
 
 	// other ....
 
@@ -146,7 +221,6 @@ function submitAuction() {
 			update(response);
 		},
 		error: function(response) {
-			// ...
 			updateError(response);
 		},
 		complete: function(response) {
@@ -154,11 +228,78 @@ function submitAuction() {
 		}
 	});
 
-
 }
 
 
+function isTimeUp(){
+	console.log("count", $count);
+	return $count <= 0 ? true : false;
+}
 
+// Validate the user input
+function validateInput(input) {
+	
+	var $valid = true;
+	var $item = input.parents("tr");
+
+	var $id = $item.children("th:eq(0)").text();
+	var $name = $item.children("th:eq(1)").text();
+	var $price = $item.children("th:eq(2)").text();
+	var $yprice = $("#price{0}".f($id)).val();
+
+	var $minIncreament = S2N($("#bid_table").data("minIncreament"));
+	console.log("validateInput", $id, $name, $price, $yprice);
+
+	if ( hasInvalidCharacters($yprice) ) {
+		input.addClass("invalid_input");
+		$("#price{0}_tips".f($id)).text("Please enter a positive number.");
+		$("#price{0}_tips".f($id)).show();
+		$valid = false;
+	} else {
+		input.removeClass("invalid_input");
+		input.addClass("invalid_price");
+		$("#price{0}_tips".f($id)).hide();
+
+		if ( S2N($yprice) < S2N($price) + $minIncreament ) {
+			input.addClass("invalid_price");
+	
+			$("#price{0}_tips".f($id)).html("Your are aborting this bid.");
+			$("#price{0}_tips".f($id)).show();
+			$valid = false;
+		} 
+	}
+	return $valid;
+
+	function hasInvalidCharacters(str) {
+		return str.search(/[^0-9]/) != -1 ;
+
+	}
+	
+}
+
+function validateAllInput() {
+	console.log("validateAllInput");
+	var $valid = true;
+	
+	$(".input_price").each(function(i) {
+		if ( validateInput($(this)) === false ) {
+			$valid = false;
+		}
+	});
+
+	console.log("All", $valid, $count);
+	return $valid;
+}
+
+function setAll2Valid() {
+	console.log("setAll2Valid");
+	
+	$(".input_price").each(function(i) {
+		if ( ! validateInput($(this)) ) {
+			$(this).val(0);
+		}
+	});
+}
 
 
 
