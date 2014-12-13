787834d9fbfa3e68c92430e829274fd9e72e4450
Xing <x.fan.cn@gmail.com>
2014-12-13 19:10:22 +1300

    finish shell generating class

---
+++ src/wdp/ExecuteAMPLComand.java
@@ -0,0 +1,54 @@
+package wdp;
+ 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+ 
+public class ExecuteAMPLComand {
+	
+	public static String amplFolderPath = "C:\\AMPL\\";
+	public static String wdpRunFile = "wdp.run";
+ 
+	public static void main(String[] args) {
+		getAnsFile();
+	}
+	
+	public static File getAnsFile() {
+		deleteOldFiles();
+		File ansFile;
+		String command = amplFolderPath + " " + wdpRunFile;
+		executeCommand(command);
+		ansFile = new File(amplFolderPath + "wdp.ans");
+		return ansFile;
+	}
+	
+	private static void deleteOldFiles() {
+		try{
+    		File oldansfile = new File(amplFolderPath + "wdp.ans");
+    		oldansfile.delete();
+    	}
+		catch(Exception e){
+    		e.printStackTrace();
+    	}
+	}
+ 
+	private static String executeCommand(String command) {
+ 
+		StringBuffer output = new StringBuffer();
+		Process p;
+		
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = "";			
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return output.toString(); 
+	}
+ 
+}
